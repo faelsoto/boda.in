1ffa518c73a37bc2eb30c8c1ad0c40439e3e8922
Rafael Soto <rafael@outofthebox.mx>
2012-02-18 09:59:25 -0600

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Novias::Application.config.secret_token = 'c72259d1c13e38357f676d0196bd3c99c48d3eaa656d99d7eaa20fb6d4c79055a585b5805da80a417dbebde200405ced0e68f2491eccf78c6d5d99c6faabc9d1'
