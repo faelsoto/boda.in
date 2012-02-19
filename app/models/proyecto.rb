fb04201e1c1c48712632dcb0b5d022cba4350d67
Rafael Soto <rafael@outofthebox.mx>
2012-02-19 10:33:45 -0600

    Proyecto: Generando modelo, probando validación

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,4 +1,9 @@
 class Proyecto < ActiveRecord::Base
+  validates :novia, :length => { :minimum => 3, :wrong_length => 'test' }
+  validates :novio, :length => { :minimum => 3, :wrong_length => 'test' }
+  validates :presupuesto, :length => { :minimum => 3, :wrong_length => 'test' }
+  validates :fecha, :length => { :minimum => 3, :wrong_length => 'test' }
+  
   def self.has? user_id
     Proyecto.where(:user_id => user_id).count.blank?
   end
